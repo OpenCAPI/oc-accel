95bf60d42765b6eec99a17bcf6924ca3e372ee33
Lu Yong <luyong@cn.ibm.com>
2018-12-24 15:51:51 +0800

    Deployed 3e19b14 with MkDocs version: 1.0.4

--- extra.js
+++ extra.js
@@ -1,17 +1,7 @@
 (function(){
 hljs.configure({languages: []});
 //hljs.initHighlightingOnLoad();
-//rest of the javascript code here
- //  $(".dropdown-menu li a").click(function(){
- //	alert("This is alert!");
- // });
 
-//  $('.dropdown-menu li a').click(function(){
-//	var node = document.createElement("LI");
-//	var textnode = document.createTextNode($(this).text()); 
-//	node.appendChild(textnode);
-//	document.getElementsByClassName("main active").appendChild(node);
-//  });
  /*   $('.dropdown').each(function (key, dropdown) {
         var $dropdown = $(dropdown);
         $dropdown.find('.dropdown-menu a').on('click', function () {
