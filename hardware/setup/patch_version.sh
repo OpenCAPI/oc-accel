ad56a877893e7a9f472105aea2082e031acc8458
Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>
2019-11-06 14:51:54 +0100

    solving ID recovery issue
    Signed-off-by: Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>

--- hardware/setup/patch_version.sh
+++ hardware/setup/patch_version.sh
@@ -48,8 +48,8 @@ sed -i "s/$SRC/$DST/" $1/$2
 #Patch HLS Action type and Release version
 #Be very careful, look for string ACTION_TYPE and RELEASE_LEVEL in $ACTION_ROOT
 if [ "$HLS_SUPPORT" == "TRUE" ]; then
-   HLS_ACTION_TYPE=`find $ACTION_ROOT -name *.[hH] | xargs grep "#define\s\+ACTION_TYPE" | cut -d "x" -f 2`
-   HLS_RELEASE_LEVEL=`find $ACTION_ROOT -name *.[hH] | xargs grep "#define\s\+RELEASE_LEVEL" | cut -d "x" -f 2`
+   HLS_ACTION_TYPE=`find $ACTION_ROOT -name *.[hH] | xargs grep "#define\s\+ACTION_TYPE" | awk -F"0x" '{print $2}' 
+   HLS_RELEASE_LEVEL=`find $ACTION_ROOT -name *.[hH] | xargs grep "#define\s\+RELEASE_LEVEL" | awk -F"0x" '{print $2}'
    echo "ACTION_TYPE is $HLS_ACTION_TYPE, RELEASE_LEVEL is $HLS_RELEASE_LEVEL"
 
    SRC="define HLS_ACTION_TYPE 32'h.*"
