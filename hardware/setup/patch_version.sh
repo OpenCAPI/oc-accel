f44b10d8ee559a5fda146c04335f31ed7c47b2c4
Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>
2019-11-14 20:25:56 +0100

    previous jenkins_mods recovery
    Signed-off-by: Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>

--- hardware/setup/patch_version.sh
+++ hardware/setup/patch_version.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-##
-## Copyright 2019 International Business Machines
-##
-## Licensed under the Apache License, Version 2.0 (the "License");
-## you may not use this file except in compliance with the License.
-## You may obtain a copy of the License at
-##
-##     http://www.apache.org/licenses/LICENSE-2.0
-##
-## Unless required by applicable law or agreed to in writing, software
-## distributed under the License is distributed on an "AS IS" BASIS,
-## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-## See the License for the specific language governing permissions and
-## limitations under the License.
-##
+#
+# Copyright 2016, International Business Machines
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
 set -e
 NAME=`basename $2`
 
@@ -47,8 +48,8 @@ sed -i "s/$SRC/$DST/" $1/$2
 #Patch HLS Action type and Release version
 #Be very careful, look for string ACTION_TYPE and RELEASE_LEVEL in $ACTION_ROOT
 if [ "$HLS_SUPPORT" == "TRUE" ]; then
-   HLS_ACTION_TYPE=`find $ACTION_ROOT -name *.[hH] | xargs grep "#define\s\+ACTION_TYPE" | cut -d "x" -f 2`
-   HLS_RELEASE_LEVEL=`find $ACTION_ROOT -name *.[hH] | xargs grep "#define\s\+RELEASE_LEVEL" | cut -d "x" -f 2`
+   HLS_ACTION_TYPE=`find $ACTION_ROOT -name *.[hH] | xargs grep "#define\s\+ACTION_TYPE" | awk -F"0x" '{print $2}'`
+   HLS_RELEASE_LEVEL=`find $ACTION_ROOT -name *.[hH] | xargs grep "#define\s\+RELEASE_LEVEL" | awk -F"0x" '{print $2}'`
    echo "ACTION_TYPE is $HLS_ACTION_TYPE, RELEASE_LEVEL is $HLS_RELEASE_LEVEL"
 
    SRC="define HLS_ACTION_TYPE 32'h.*"
