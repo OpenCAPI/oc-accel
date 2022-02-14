c5618b2bc888e9b7e83bff609c0f35a333bb84ad
fmoyen <fabrice_moyen@fr.ibm.com>
2022-02-14 14:52:21 +0100

    soft_reload.sh: updated to be generic

--- software/tools/soft_reload.sh
+++ software/tools/soft_reload.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ##
 ## Copyright 2019 International Business Machines
 ##
@@ -14,31 +16,151 @@
 ## limitations under the License.
 ##
 
-echo "##-------------------------------------------------------##"
-echo "## This sequence is to set the reload bit to 1 so that   ##"
-echo "##  at next oc-reset the FPGA is reloaded from the Flash ##"
-echo "##-------------------------------------------------------##"
+###################################################################################################################################
+# VARIABLES
 
-echo "check that ICAP is ready for programming: Expected read value is 0x00000005"
-./snap_peek -C5 -w32 0x0F10
-echo "check that WR fifo is empty (3f empty lines) Expected read value is 0x0000003f"
+Option=""
+CardID=""
+FullPathScript=`realpath $0`
+FullPathDir=`dirname $FullPathScript`
+
+###################################################################################################################################
+# FUNCTIONS
+#
+
+function usage
+{
+  echo
+  echo "`basename $0` Usage:"
+  echo "-------------------------"
+  echo
+  echo "  + The goal of this script is to set the reload bit of the OpenCAPI card to 1 so that"
+  echo "    at next oc-reset the FPGA chip of the card is reloaded from the flash memory"
+  echo
+  echo "  + The script must be used when the ICAP IP is loaded into the User Space (and not the Configuration Space)"
+  echo "    This is the case when addressing the OpenCAPI card from a container (pod) in a cloud (Partial Reconfiguration)"
+  echo
+  echo
+  echo "  -C <Card ID>   : (mandatory) The Position/ID of the card (use 'oc_find-card -v -AALL' to get the card Position/ID)"
+  echo "  -h             : (optional) shows this usage info"
+  echo
+  exit 0
+}
+
+
+###################################################################################################################################
+# MAIN
+#
+
+#----------------------------------------------------------------------------------------------------------------------------------
+# GETOPTS
+
+while getopts ":C:h" Option
+do
+  case $Option in
+    C )
+      CardID=$OPTARG
+    ;;
+
+    h )
+      usage
+    ;;
+
+    \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+    :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+    *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+
+  esac
+done    
+
+if [ -z "$CardID" ]; then
+  echo 
+  echo "ERROR:"
+  echo "------"
+  echo "PROVIDING THE POSITION/ID OF THE CARD IS MANDATORY."
+  usage
+fi
+
+echo "##-----------------------------------------------------------------------##"
+echo "## The sequence below is to set the reload bit to 1 so that              ##"
+echo "##  at next oc-reset the FPGA is reloaded from the Flash                 ##"
+echo "##-----------------------------------------------------------------------##"
+
+echo
+echo "##-----------------------------------------------------------------------##"
+echo "## Checking that ICAP is ready for programming                           ##"
+echo "## (Expected read value is 0x00000005)                                   ##"
+
+if $FullPathDir/snap_peek -C $CardID -w32 0x0F10 -e 0x00000005; then
+  echo "The ICAP is ready for programming --> Let's continue"
+else
+  echo
+  echo "ERROR: The ICAP is NOT ready for programming."
+  echo "Exiting..."
+  echo
+  exit 2
+fi
+
+echo
+echo "##-----------------------------------------------------------------------##"
+echo "## checking that WR fifo is empty (0x3f empty lines)                     ##"
+echo "## (Expected read value is 0x0000003f)                                   ##"
+
+if $FullPathDir/snap_peek -C $CardID -w32 0x0F14 -e 0x0000003f; then
+  echo "The WR fifo is empty --> Let's continue"
+else
+  echo
+  echo "ERROR: The WR fifo is NOT empty."
+  echo "Exiting..."
+  echo
+  exit 3
+fi
+
+echo
+echo "##-----------------------------------------------------------------------##"
+echo "## Pushing the reload sequence in WR fifo                                ##"
+
+$FullPathDir/snap_poke -C $CardID -w32 0x0F00 0xFFFFFFFF
+$FullPathDir/snap_poke -C $CardID -w32 0x0F00 0xAA995566
+$FullPathDir/snap_poke -C $CardID -w32 0x0F00 0x20000000
+$FullPathDir/snap_poke -C $CardID -w32 0x0F00 0x30020001
+$FullPathDir/snap_poke -C $CardID -w32 0x0F00 0x00000000
+$FullPathDir/snap_poke -C $CardID -w32 0x0F00 0x30080001
+$FullPathDir/snap_poke -C $CardID -w32 0x0F00 0x0000000F
+$FullPathDir/snap_poke -C $CardID -w32 0x0F00 0x20000000
+
+echo
+echo "##-----------------------------------------------------------------------##"
+echo "## checking that WR fifo is NOT empty (0x37 empty lines)                 ##"
+echo "## (Expected read value is 0x00000037)                                   ##"
+
+if $FullPathDir/snap_peek -C $CardID -w32 0x0F14 -e 0x00000037; then
+  echo "The WR fifo is not empty anymore (expected 0x37 empty lines) --> Let's continue"
+else
+  echo
+  echo "ERROR: The WR fifo has not been correctly loaded."
+  echo "Exiting..."
+  echo
+  exit 4
+fi
+
+echo
+echo "##-----------------------------------------------------------------------##"
+echo "## Flushing the FIFO to set the reload bit to 1                          ##"
+$FullPathDir/snap_poke -C $CardID -w32 0x0F0C 0x00000001
+
+echo
+echo "##-----------------------------------------------------------------------##"
+echo "## Registers should not be readable anymore                              ##"
+echo "## (Expected read value is 0xffffffff)                                   ##" 
 ./snap_peek -C5 -w32 0x0F14
 
-echo "Reload sequence pushed in WR fifo"
-./snap_poke -C5 -w32 0x0F00 0xFFFFFFFF
-./snap_poke -C5 -w32 0x0F00 0xAA995566
-./snap_poke -C5 -w32 0x0F00 0x20000000
-./snap_poke -C5 -w32 0x0F00 0x30020001
-./snap_poke -C5 -w32 0x0F00 0x00000000
-./snap_poke -C5 -w32 0x0F00 0x30080001
-./snap_poke -C5 -w32 0x0F00 0x0000000F
-./snap_poke -C5 -w32 0x0F00 0x20000000
-
-echo "check that WR fifo is NOT empty (37 empty lines) Expected read value is 0x00000037"
-./snap_peek -C5 -w32 0x0F14
-
-echo "Flush the FIFO to set the reload bit to 1"
-./snap_poke -C5 -w32 0x0F0C 0x00000001
-
-echo "Registers should not be readable anymore: expected 0xffffffff"
-./snap_peek -C5 -w32 0x0F14
+if $FullPathDir/snap_peek -C $CardID -w32 0x0F14 -e 0xffffffff; then
+  echo "Registers are not readable anymore --> Now, the card should be reloaded at next oc-reset."
+else
+  echo
+  echo "ERROR: Registers are still readable, which should not be the case anymore."
+  echo "Exiting..."
+  echo
+  exit 5
+fi
