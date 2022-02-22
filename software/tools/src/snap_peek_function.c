01862b67f19ec1bbc38c900a700d66e1e7b4072f
fmoyen <fabrice_moyen@fr.ibm.com>
2022-02-22 16:44:06 +0100

    first try with snap_peek_function

---
+++ software/tools/src/snap_peek_function.c
@@ -0,0 +1,303 @@
+/*
+ * Copyright 2019 International Business Machines
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#include <osnap_tools.h>
+#include <libosnap.h>
+#include "force_cpu.h"
+
+int verbose_flag = 0;
+
+static const char* version = GIT_VERSION;
+
+/**
+ * @brief        prints valid command line options
+ *
+ * @param prog        current program's name
+ */
+static void usage (const char* prog)
+{
+    printf ("Usage: %s [-h] [-v,--verbose]\n"
+            "  -C,--card <cardno>        use 'oc_find-card -v -AALL' to get the card number\n"
+            "  -V, --version             print version.\n"
+            "  -q, --quiet               quiece output.\n"
+            "  -w, --width <32|64>       access width, 64: default\n"
+            "  -X, --cpu <id>            only run on this CPU.\n"
+            "  -i, --interval <intv>     interval in usec, 0: default.\n"
+            "  -c, --count <num>         number of peeks do be done, 1: default.\n"
+            "  -e, --must-be <value>     compare and exit if not equal.\n"
+            "  -n, --must-not-be <value> compare and exit if equal.\n"
+            "  -d, --dump                Number of 32 or 64 bytes to read. default 1\n"
+            "  <addr>\n"
+            "Note: Use -w32 to access snap action starting at offset 0x10000\n"
+            "Example:\n"
+            "Read OC global BDR register (Build date register):\n"
+            "  $ snap_peek -C0 0x0008\n"
+            "  [00000008]  0000202011181128\n\n"
+            "Read OC action ATR register (Action ID):\n"
+            "  $ snap_peek -C0 -w32 0x0010\n"
+            "  [00000010] 10143009\n\n",
+            prog);
+}
+
+/**
+ * Read accelerator specific registers. Must be called as root!
+ */
+int main (int argc, char* argv[])
+{
+    int ch, rc = 0;
+    int card_no = 0;
+    struct snap_card* card;
+    int cpu = -1;
+    int width = 64;
+    uint32_t offs;
+    uint64_t val = 0xffffffffffffffffull;
+    uint64_t and_mask = 0xffffffffffffffffull;
+    uint64_t equal_val = val;
+    uint64_t not_equal_val = val;
+    int equal = 0, not_equal = 0;
+    int quiet = 0;
+    unsigned long i, count = 1;
+    unsigned long interval = 0;
+    char device[128];
+    int dump = 1;
+
+    while (1) {
+        int option_index = 0;
+        static struct option long_options[] = {
+            /* options */
+            { "card",         required_argument, NULL, 'C' },
+            { "cpu",         required_argument, NULL, 'X' },
+
+            { "width",         required_argument, NULL, 'w' },
+            { "interval",         required_argument, NULL, 'i' },
+            { "count",         required_argument, NULL, 'c' },
+            { "must-be",         required_argument, NULL, 'e' },
+            { "must-not-be", required_argument, NULL, 'n' },
+            { "and-mask",    required_argument, NULL, 'a' },
+
+            /* misc/support */
+            { "version",         no_argument,            NULL, 'V' },
+            { "quiet",         no_argument,            NULL, 'q' },
+            { "verbose",         no_argument,            NULL, 'v' },
+            { "help",         no_argument,            NULL, 'h' },
+            { "dump",         required_argument, NULL, 'd' },
+            { 0,                 no_argument,            NULL, 0   },
+        };
+
+        ch = getopt_long (argc, argv,
+                          "C:X:w:i:c:e:n:a:d:Vqvh",
+                          long_options, &option_index);
+
+        if (ch == -1) {      /* all params processed ? */
+            break;
+        }
+
+        switch (ch) {
+        /* which card to use */
+        case 'C':
+            card_no = strtol (optarg, (char**)NULL, 0);
+            break;
+
+        case 'X':
+            cpu = strtoul (optarg, NULL, 0);
+            break;
+
+        case 'w':
+            width = strtoul (optarg, NULL, 0);
+            break;
+
+        case 'i':                /* interval */
+            interval = strtol (optarg, (char**)NULL, 0);
+            break;
+
+        case 'c':                /* loop count */
+            count = strtol (optarg, (char**)NULL, 0);
+            break;
+
+        case 'e':
+            equal = 1;
+            equal_val = strtoull (optarg, NULL, 0);
+            break;
+
+        case 'n':
+            not_equal = 1;
+            not_equal_val = strtoull (optarg, NULL, 0);
+            break;
+
+        case 'a':
+            and_mask = strtoull (optarg, NULL, 0);
+            break;
+
+        case 'V':
+            printf ("%s\n", version);
+            exit (EXIT_SUCCESS);
+
+        case 'q':
+            quiet++;
+            break;
+
+        case 'v':
+            verbose_flag++;
+            break;
+
+        case 'h':
+            usage (argv[0]);
+            exit (EXIT_SUCCESS);
+            break;
+
+        case 'd':                /* dump */
+            dump = strtol (optarg, (char**)NULL, 0);
+            break;
+
+        default:
+            usage (argv[0]);
+            exit (EXIT_FAILURE);
+        }
+    }
+
+    if (optind + 1 != argc) {
+        usage (argv[0]);
+        exit (EXIT_FAILURE);
+    }
+
+    offs = strtoull (argv[optind], NULL, 0);
+
+    if (equal && not_equal) {
+        usage (argv[0]);
+        exit (EXIT_FAILURE);
+    }
+
+    switch_cpu (cpu, verbose_flag);
+
+    if (card_no == 0) {
+        snprintf (device, sizeof (device) - 1, "IBM,oc-snap");
+    } else {
+        snprintf (device, sizeof (device) - 1, "/dev/ocxl/IBM,oc-snap.000%d:00:00.1.0", card_no);
+    }
+
+    if (verbose_flag) {
+        printf ("[%s] Open CAPI Card: %s\n", argv[0], device);
+    }
+
+    card = snap_card_alloc_dev (device, SNAP_VENDOR_ID_ANY,
+                                SNAP_DEVICE_ID_ANY);
+
+    if (card == NULL) {
+        fprintf (stderr, "err: failed to open card %u: %s\n", card_no,
+                 strerror (errno));
+        exit (EXIT_FAILURE);
+    }
+
+    if (verbose_flag) {
+        printf ("[%s] Open CAPI Card Got handle: %p\n", argv[0], card);
+    }
+
+    for (i = 0; i < count; i++) {
+dump_more:
+
+        switch (width) {
+        case 32: {
+            if (verbose_flag > 1)
+                printf ("[%s] snap_action_read32(%p, %x)\n",
+                        argv[0], card, offs);
+
+            rc = snap_action_read32 (card, offs, (uint32_t*)&val);
+            val &= 0xffffffff; /* mask off obsolete bits ... */
+            break;
+        }
+
+        default:
+        case 64:
+            if (verbose_flag > 1)
+                printf ("[%s] snap_global_read64(%p, %x)\n",
+                        argv[0], card, offs);
+
+            rc = snap_global_read64 (card, offs, &val);
+            break;
+        }
+
+        if (rc != 0) {
+            fprintf (stderr, "err: could not read [%08x] rc=%d\n",
+                     offs, rc);
+            snap_card_free (card);
+            exit (EXIT_FAILURE);
+        }
+
+        if ((equal) &&
+            (equal_val != (val & and_mask))) {
+            fprintf (stderr, "err: [%08x] %016llx != %016llx\n",
+                     offs, (long long)val, (long long)equal_val);
+            snap_card_free (card);
+            exit (EX_ERR_DATA);
+        }
+
+        if ((not_equal) &&
+            (not_equal_val == (val & and_mask))) {
+            fprintf (stderr, "err: [%08x] %016llx == %016llx\n",
+                     offs, (long long)val,
+                     (long long)not_equal_val);
+            snap_card_free (card);
+            exit (EX_ERR_DATA);
+        }
+
+        if (interval) {
+            usleep (interval);
+        }
+
+        dump--;
+
+        if (dump >= 1) {
+            if (32 == width) {
+                printf ("[%08x] %08lx\n", offs, (long)val);
+                offs += 4;
+            } else {
+                printf ("[%08x] %016llx\n", offs, (long long)val);
+                offs += 8;
+            }
+
+            goto dump_more;
+        }
+    }
+
+    if (verbose_flag) {
+        printf ("[%s] Close CAPI Card: %p\n", argv[0], card);
+    }
+
+    snap_card_free (card);
+
+    if (!quiet) {
+        if (32 == width) {
+            printf ("[%08x] %08lx\n", offs, (long)val);
+        } else {
+            printf ("[%08x] %016llx\n", offs, (long long)val);
+        }
+    }
+
+    if (verbose_flag) {
+        printf ("[%s] Exit rc %d\n", argv[0], rc);
+    }
+
+    exit (EXIT_SUCCESS);
+}
---
+++ software/tools/src/snap_poke_function.c
@@ -0,0 +1,252 @@
+/*
+ * Copyright 2019 International Business Machines
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <endian.h>
+#include <asm/byteorder.h>
+#include <sys/mman.h>
+#include <getopt.h>
+#include <osnap_tools.h>
+#include <libosnap.h>
+#include "force_cpu.h"
+
+int verbose_flag = 0;
+static int quiet = 0;
+static const char* version = GIT_VERSION;
+
+/**
+ * @brief Prints valid command line options
+ *
+ * @param prog        current program name
+ */
+static void usage (const char* prog)
+{
+    printf ("Usage: %s [-h] [-v,--verbose]\n"
+            "  -C,--card <cardno>        use 'oc_find-card -v -AALL' to get the card number\n"
+            "  -V, --version             print version.\n"
+            "  -q, --quiet               quiece output.\n"
+            "  -w, --width <32|64>       access width, 64: default\n"
+            "  -X, --cpu <id>            only run on this CPU.\n"
+            "  -i, --interval <intv>     interval in usec, 0: default.\n"
+            "  -c, --count <mum>         number of pokes, 1: default\n"
+            "  -r, --read-back           read back and verify.\n"
+            "  <addr> <val>\n"
+            "\n"
+            "Note: Use -w32 to access snap action starting at offset 0x10000\n"
+            "Example:\n"
+            "Write OC global BDR register (Build date register):\n"
+            "  $ snap_poke -C0 0x0008 0xdeadbeef\n"
+            "Write OC action ATR register (Action ID):\n"
+            "  $ snap_poke -C0 -w32 0x0010 0xdeadbeef\n"
+            "\n",
+            prog);
+}
+
+/**
+ * @brief Tool to write to zEDC registers. Must be called as root!
+ */
+int main (int argc, char* argv[])
+{
+    int ch, rc, rbrc = 0;
+    int card_no = 0;
+    struct snap_card* card;
+    int cpu = -1;
+    int width = 64;
+    int rd_back = 0;
+    uint32_t offs;
+    uint64_t val, rbval;
+    unsigned long i, count = 1;
+    unsigned long interval = 0;
+    int xerrno;
+    char device[128];
+
+    while (1) {
+        int option_index = 0;
+        static struct option long_options[] = {
+            /* options */
+            { "card",        required_argument, NULL, 'C' },
+            { "cpu",        required_argument, NULL, 'X' },
+
+            { "width",        required_argument, NULL, 'w' },
+            { "interval",        required_argument, NULL, 'i' },
+            { "count",        required_argument, NULL, 'c' },
+            { "rd-back",        no_argument,       NULL, 'r' },
+
+            /* misc/support */
+            { "version",        no_argument,           NULL, 'V' },
+            { "quiet",        no_argument,           NULL, 'q' },
+            { "verbose",        no_argument,           NULL, 'v' },
+            { "help",        no_argument,           NULL, 'h' },
+
+            { 0,                no_argument,           NULL, 0   },
+        };
+
+        ch = getopt_long (argc, argv, "p:C:X:w:i:c:Vqrvh",
+                          long_options, &option_index);
+
+        if (ch == -1) {      /* all params processed ? */
+            break;
+        }
+
+        switch (ch) {
+        /* which card to use */
+        case 'C':
+            card_no = strtol (optarg, (char**)NULL, 0);
+            break;
+
+        case 'X':
+            cpu = strtoul (optarg, NULL, 0);
+            break;
+
+        case 'w':
+            width = strtoul (optarg, NULL, 0);
+            break;
+
+        case 'i':                /* interval */
+            interval = strtol (optarg, (char**)NULL, 0);
+            break;
+
+        case 'c':                /* loop count */
+            count = strtol (optarg, (char**)NULL, 0);
+            break;
+
+        case 'V':
+            printf ("%s\n", version);
+            exit (EXIT_SUCCESS);
+
+        case 'q':
+            quiet++;
+            break;
+
+        case 'r':
+            rd_back++;
+            break;
+
+        case 'v':
+            verbose_flag++;
+            break;
+
+        case 'h':
+            usage (argv[0]);
+            exit (EXIT_SUCCESS);
+            break;
+
+        default:
+            usage (argv[0]);
+            exit (EXIT_FAILURE);
+        }
+    }
+
+    if (optind + 2 != argc) {
+        usage (argv[0]);
+        exit (EXIT_FAILURE);
+    }
+
+    offs = strtoull (argv[optind++], NULL, 0);
+    val  = strtoull (argv[optind++], NULL, 0);
+    rbval = ~val;
+    switch_cpu (cpu, verbose_flag);
+
+    if ((card_no < 0) || (card_no > 7)) {
+        fprintf (stderr, "err: (%d) is a invalid card number!\n",
+                 card_no);
+        usage (argv[0]);
+        exit (EXIT_FAILURE);
+    }
+
+    if (card_no == 0) {
+        snprintf (device, sizeof (device) - 1, "IBM,oc-snap");
+    } else {
+        snprintf (device, sizeof (device) - 1, "/dev/ocxl/IBM,oc-snap.000%d:00:00.1.0", card_no);
+    }
+
+    card = snap_card_alloc_dev (device, SNAP_VENDOR_ID_ANY,
+                                SNAP_DEVICE_ID_ANY);
+
+    if (card == NULL) {
+        fprintf (stderr, "err: failed to open card %u: %s\n", card_no,
+                 strerror (errno));
+        exit (EXIT_FAILURE);
+    }
+
+    for (i = 0; i < count; i++) {
+        switch (width) {
+        case 32:
+            rc = snap_action_write32 (card, offs, (uint32_t)val);
+            xerrno = errno;
+
+            if (rd_back)
+                rbrc = snap_action_read32 (card, offs,
+                                           (uint32_t*)&rbval);
+
+            break;
+
+        default:
+        case 64:
+            printf ("Poke SNAP Global regs: 0x%x, 0x%lx\n", offs, val);
+            rc = snap_global_write64 (card, offs, val);
+            xerrno = errno;
+
+            if (rd_back) {
+                rbrc = snap_global_read64 (card, offs, &rbval);
+            }
+
+            break;
+        }
+
+        if (rc != 0) {
+            fprintf (stderr, "err: could not write "
+                     "%016llx to [%08x]\n"
+                     "  %s\n", (unsigned long long)val, offs,
+                     strerror (xerrno));
+            snap_card_free (card);
+            exit (EXIT_FAILURE);
+        }
+
+        if (rd_back) {
+            if (rbrc != 0) {
+                fprintf (stderr, "err: read back failed (%d)\n",
+                         rbrc);
+                snap_card_free (card);
+                exit (EXIT_FAILURE);
+            }
+
+            if (val != rbval) {
+                fprintf (stderr, "err: post verify failed "
+                         "%016llx/%016llx\n",
+                         (unsigned long long)val,
+                         (unsigned long long)rbval);
+                snap_card_free (card);
+                exit (EXIT_FAILURE);
+            }
+        }
+
+        if (interval) {
+            usleep (interval);
+        }
+    }
+
+    snap_card_free (card);
+
+    if (!quiet) {
+        printf ("[%08x] %016llx\n", offs, (long long)val);
+    }
+
+    exit (EXIT_SUCCESS);
+}
